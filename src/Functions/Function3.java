e38aa6f35bd780f11f52f1d4a425018fb9e6bcdd
unknown <huydogiaccac@gmail.com>
2023-12-18 07:39:57 +0700

    Huy test

--- src/Functions/Function3.java
+++ src/Functions/Function3.java
@@ -4,6 +4,9 @@ public class Function3 {
 
 	public Function3() {
 		// TODO Auto-generated constructor stub
+		
 	}
-
+	public static void main(String[] args) {
+		System.out.println("Quan dep trai");
+	}
 }
